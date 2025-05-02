8652601529280b58e9a0b1e6e5bc6d355aac3ef8
Israel Olubode <olubode777@gmail.com>
2025-05-02 20:14:17 +0100

    Fix case-sensitive import for netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
-import Home from './pages/Home page/Home'
-import Packages from './pages/Packages Page/Packages'
-import Destinations from './pages/Destinations Page/Destinations'
-import Contact from './pages/Contact Page/Contact'
+import Home from './pages/HomePage/Home'
+import Packages from './pages/PackagesPage/Packages'
+import Destinations from './pages/DestinationsPage/Destinations'
+import Contact from './pages/ContactPage/Contact'
 // import Blog from './pages/Blog Page/Blog'
 import Nav from './components/Navbar/Nav'
 import Footer from './components/Footer/Footer'
